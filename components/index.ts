bcd34252e4f9800b6f66eee9b3c6dfb5fd64a622
Max Strikanov <maxvell.88@mail.ru>
2022-01-27 17:24:56 +0300

    new branch

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,5 @@ export * from './Htag/Htad';
 export * from './Button/Button';
 export * from './Ptag/Ptag';
 export * from './Tag/Tag';
+export * from './Rating/Rating';
+export * from '../layout/Layout'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { Button, Htag, Ptag, Tag } from '../components';
+import React, { useEffect, useState } from 'react';
+import { Button, Htag, Ptag, Tag, Rating } from '../components';
 
 
 export default function Home(): JSX.Element {
+
+    const [rating, setRating] = useState<number>(4);
+
     return (
-        < >
-            <Htag tag='h1'>Text</Htag>
-            <Button appearence='primary' arrow='right'>Button</Button>
-            <Button appearence='ghost' arrow='right'>Button</Button>
+        <>
+            <Htag tag='h1'>Заголовок</Htag>
+            <Button appearence='primary' arrow='right' >Button</Button>
+            <Button appearence='ghost' arrow='right' >Button</Button>
             <Ptag size = 'l'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus tempora cupiditate laboriosam quis nihil rerum officiis molestias magnam dicta. Assumenda similique eligendi qui non eaque vero placeat voluptatibus enim animi.</Ptag>
             <Tag size='s' >Ghost</Tag>
             <Tag size='m' color='red'>Red</Tag>
-            <Tag size='s' color='green'>Red</Tag>
+            <Tag size='s' color='green'>Green</Tag>
             <Tag size='s' color='primary'>Primary</Tag>
+            <Rating rating={rating} isEditable setRating={setRating}/>
         </>
 );
 }
